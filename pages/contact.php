f361b6a0c39b1e2f57711d0bb7460aaa6cc811fd
Mbrenn <brenn.makouya@ynov.com>
2024-10-01 12:11:25 +0200

    add new css

--- pages/contact.php
+++ pages/contact.php
@@ -1,15 +1,21 @@
 <?php include '../includes/header.php'; ?>
-<main>
-    <h1>Contact Me</h1>
-    <form action="send_mail.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Send</button>
+<main class="container">
+    <h1 class="mt-5">Contact Me</h1>
+    <form action="send_mail.php" method="post" class="mt-4">
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" class="form-control" required></textarea>
+        </div>
+        <button type="submit" class="btn">Send</button>
     </form>
-    <div id="map"></div>
+    <div id="map" class="mt-4"></div>
 </main>
 <?php include '../includes/footer.php'; ?>
